fb8bf775f33a33a0210f323af4f970d2ae081955
junior <=>
2022-03-04 21:43:32 -0300

    Sistema de autenticação concluído

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
+from django.urls import path, include
+from . import views 
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
 ]
