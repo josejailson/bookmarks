4d0bf6113f4346e51d6d71b605829aea75fc4e9c
junior <=>
2022-03-05 14:01:05 -0300

    Agora, cada usuário tem sua própria página de perfil

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from . import views 
+from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
